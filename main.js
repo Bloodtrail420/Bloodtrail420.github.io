a95c2694e87102066d3a589441538a6f1b733f72
Jahu420 <dekowskij@gmail.com>
2026-06-19 00:17:45 +0200

    Add files via upload

---
+++ main.js
@@ -0,0 +1,133 @@
+function PokazKlatka(){
+    const info = document.getElementById("informacje");
+
+    info.innerHTML =`
+    <strong>Mięśnie klatki piersiowej, Ich głównym zadaniem jest poruszanie kończynami górnymi oraz wspomaganie oddychania.</strong> <br> 
+    <a href=https://www.anatomik.pl/lekcje/miesnie-powierzchowne-klatki-piersiowej/ target=_blank>Więcej Informacji.</a>
+`
+
+}
+function PokazBoczne(){
+    const info = document.getElementById("informacje");
+
+    info.innerHTML =`
+    <strong>Mięśnie skośne brzucha, odpowiadają za rotacje tułowia, zginanie boczne oraz stabilizacje kręgosłupa podczas ruchów.</strong>  <br>
+    <a href=https://fizjoterapeuty.pl/uklad-miesniowy/miesien-skosny-wewnetrzny-brzucha.html target=_blank>Więcej Informacji.</a>`
+}
+
+function PokazProste(){
+    const info = document.getElementById("informacje");
+
+    info.innerHTML =`<strong>Mięśnie proste brzucha, służą do zginania tułowia w przód oraz ochrone organów wewnętrznych.</strong> <br> <a href=https://fizjoterapeuty.pl/uklad-miesniowy/miesien-prosty-brzucha.html target=_blank>Więcej Informacji.</a>`
+}
+
+
+function PokazBiceps(){
+    const info = document.getElementById("informacje");
+
+    info.innerHTML =`
+    <strong>Mięsień dwugłowy ramienia (biceps), służy do zginania przedramienia oraz obracania dłoni do góry.</strong> <br> <a href=https://www.anatomik.pl/lekcje/miesien-dwuglowy-ramienia/ target=_blank>Więcej Informacji.</a>`
+}
+
+function PokazTriceps(){
+    const info= document.getElementById("informacje");
+
+    info.innerHTML =`<strong>Mięsień trójgłowy ramienia (triceps), odpowiada za prostowanie przedramienia w stawie łokciowym.</strong> <br> <a href=https://www.anatomik.pl/lekcje/miesien-trojglowy-ramienia/ target=_blank>Więcej Informacji.</a>`
+}
+
+function PokazSzyje(){
+    const info = document.getElementById("informacje");
+
+    info.innerHTML = `<strong>Mięśnie szyi, odpowiadają za ruchy głowy (zginanie, prostowanie, rotacja, zginanie boczne) oraz stabilizację szyjnego odcinka kręgosłupa.</strong> <br> <a href=https://www.anatomik.pl/lekcje/miesnie-szyi/ target=_blank>Więcej Informacji.</a>`
+}
+function PokazTrapezoidy(){
+    const info = document.getElementById("informacje");
+
+    info.innerHTML = `<strong>Mięsień czworoboczny (trapezius), odpowiada za unoszenie, zbliżanie i obniżanie łopatki oraz prostowanie i rotację głowy.</strong> <br> <a href=https://www.anatomik.pl/lekcje/miesien-czworoboczny-grzbietu/ target=_blank>Więcej Informacji.</a>`
+}
+function PokazNaramienny(){
+    const info = document.getElementById("informacje");
+
+    info.innerHTML = `<strong>Mięsień naramienny (deltoid), służy do odwodzenia ramienia, zginania i prostowania ramienia w stawie barkowym.</strong> <br> <a href= https://www.anatomik.pl/lekcje/miesien-naramienny/ target=_blank>Więcej Informacji.</a>`
+}
+function PokazCzworogłowyUda(){
+    const info = document.getElementById("informacje");
+
+    info.innerHTML = `<strong>Mięsień czworogłowy uda (quadriceps), odpowiada za prostowanie nogi w stawie kolanowym oraz stabilizację kolana podczas chodzenia i stania.</strong> <br> <a href= https://www.anatomik.pl/lekcje/miesien-czworoglowy-uda/ target=_blank>Więcej Informacji.</a>`
+}
+function PokazPrzedramieniePrzod(){
+    const info = document.getElementById("informacje");
+
+    info.innerHTML = `<strong>Mięśnie przednie przedramienia, odpowiadają za zginanie ręki i palców oraz pronację przedramienia (obracanie dłoni w dół).</strong> <br> <a href= https://www.anatomik.pl/lekcje/miesnie-przedramienia/#:~:text=1.%20Grupa%20przednia%20mi%C4%99%C5%9Bni%20przedramienia target=_blank>Więcej Informacji.</a>`
+}
+function PokazPrzedramienieTyl(){
+    const info = document.getElementById("informacje");
+
+    info.innerHTML = `<strong>Mięśnie tylne przedramienia, odpowiadają za prostowanie ręki i palców oraz supinację przedramienia (obracanie dłoni do góry).</strong> <br> <a href= https://www.anatomik.pl/lekcje/miesnie-przedramienia/#:~:text=3.%20Grupa%20tylna%20mi%C4%99%C5%9Bni%20przedramienia target=_blank>Więcej Informacji.</a>`
+}function PokazPiszczelowy(){
+    const info = document.getElementById("informacje");
+
+    info.innerHTML = `<strong>Mięsień piszczelowy przedni, służy do zginania grzbietowego stopy (unoszenie palców) oraz odwodzenia stopy, stabilizuje łuk podłużny stopy.</strong> <br> <a href= https://www.anatomik.pl/lekcje/miesnie-podudzia/#:~:text=1.%20Grupa%20przednia%20mi%C4%99%C5%9Bni%20podudzia target=_blank>Więcej Informacji.</a>`
+}
+function PokazŁydke(){
+    const info = document.getElementById("informacje");
+
+    info.innerHTML = `<strong>Mięśnie łydki (głównie brzuchaty i płaszczkowaty), odpowiadają za zginanie podeszwowe stopy (wstawanie na palce) oraz stabilizację podczas chodzenia i biegania.</strong> <br> <a href= https://www.anatomik.pl/lekcje/miesnie-podudzia/#:~:text=3.%20Grupa%20tylna%20mi%C4%99%C5%9Bni%20podudzia target=_blank>Więcej Informacji.</a>`
+}
+function PokazKolano(){
+    const info = document.getElementById("informacje");
+
+    info.innerHTML = `<strong>Staw kolanowy, służy głównie do zginania i prostowania nogi oraz umożliwia nieznaczne ruchy rotacyjne, pełni kluczową rolę w chodzeniu, bieganiu i stabilizacji ciała.</strong> <br> <a href= https://fizjoterapeuty.pl/anatomia/staw-kolanowy-anatomia-kolana.html target=_blank>Więcej Informacji.</a>`
+}
+function PokazDłoń(){
+    const info = document.getElementById("informacje");
+
+    info.innerHTML = `<strong>Dłoń, służy do precyzyjnego chwytania, manipulacji przedmiotami, wykonywania złożonych ruchów oraz zapewnia wysoko rozwinięte czucie dotyku.</strong> <br> <a href= https://fizjoterapia.elmedico.pl/Anatomia-reki-budowa-kosci-reki-stawy-i-miesnie-urazy-i-choroby-konczyny-gornej-blog-pol-1771929898.html target=_blank>Więcej Informacji.</a>`
+}
+function PokazStawSkok(){
+    const info = document.getElementById("informacje");
+
+    info.innerHTML = `<strong>Staw skokowy, odpowiada za zginanie grzbietowe i podeszwowe stopy oraz odwracanie i nawracanie, umożliwiając adaptację do terenu i amortyzację wstrząsów.</strong> <br> <a href= https://fizjoterapeuty.pl/anatomia/staw-skokowy.html target=_blank>Więcej Informacji.</a>`
+}
+function PokazStope(){
+    const info = document.getElementById("informacje");
+
+    info.innerHTML = `<strong>Stopa, pełni funkcję podpory ciała, amortyzacji wstrząsów oraz propulsji podczas chodzenia i biegania, dzięki licznych stawom, łukom i mięśniom.</strong> <br> <a href= https://fizjoterapeuty.pl/anatomia/stopa-stawy-wiezadla-i-miesnie.html target=_blank>Więcej Informacji.</a>`
+}
+function PokazNadGrzebieniowy(){
+    const info = document.getElementById("informacje");
+
+    info.innerHTML = `<strong>Mięsień nadgrzebieniowy, służy do inicjowania odwodzenia ramienia w stawie barkowym oraz stabilizacji głowy kości ramiennej.</strong> <br> <a href= https://www.anatomik.pl/lekcje/miesien-nadgrzebieniowy/ target=_blank>Więcej Informacji.</a>`
+}
+
+function PokazPodGrzebieniowy(){
+    const info = document.getElementById("informacje");
+
+    info.innerHTML = `<strong>Mięsień podgrzebieniowy, odpowiada za zewnętrzną rotację ramienia oraz stabilizację stawu barkowego.</strong> <br> <a href= https://www.anatomik.pl/lekcje/miesien-podlopatkowy/ target=_blank>Więcej Informacji.</a>`
+}
+
+function PokazNajszerszy(){
+    const info = document.getElementById("informacje");
+
+    info.innerHTML = `<strong>Mięsień najszerszy grzbietu, odpowiada za przyciąganie ramienia do tułowia (addukcja), prostowanie ramienia oraz wewnętrzną rotację.</strong> <br> <a href= https://www.anatomik.pl/lekcje/miesien-najszerszy-grzbietu/. target=_blank>Więcej Informacji.</a>`
+}
+function PokazProstownik(){
+    const info = document.getElementById("informacje");
+
+    info.innerHTML = `<strong>Mięsień biodrowo-żebrowy, odpowiada za prostowanie tułowia, zginanie boczne kręgosłupa oraz stabilizację odcinka lędźwiowego i piersiowego.</strong> <br> <a href= https://www.anatomik.pl/lekcje/miesien-biodrowo-zebrowy/. target=_blank>Więcej Informacji.</a>`
+}
+function PokazPośDuży(){
+    const info = document.getElementById("informacje");
+
+    info.innerHTML = `<strong>Mięsień pośladkowy wielki, służy do prostowania stawu biodrowego (np. wstawanie, wspinanie, bieganie), zewnętrznej rotacji uda oraz stabilizacji miednicy.</strong> <br> <a href= https://www.anatomik.pl/lekcje/miesien-posladkowy-wielki/. target=_blank>Więcej Informacji.</a>`
+}
+function PokazPośMał(){
+    const info = document.getElementById("informacje");
+
+    info.innerHTML = `<strong>Mięsień pośladkowy mały, odpowiada za odwodzenie uda w stawie biodrowym, wewnętrzną rotację oraz stabilizację miednicy podczas chodzenia (zapobiega opadaniu miednicy).</strong> <br> <a href= https://www.anatomik.pl/lekcje/miesien-posladkowy-maly/. target=_blank>Więcej Informacji.</a>`
+}
+function PokażDwuGłowyUda(){
+    const info = document.getElementById("informacje");
+
+    info.innerHTML = `<strong>Mięsień dwugłowy uda, odpowiada za zginanie nogi w stawie kolanowym, prostowanie w stawie biodrowym oraz zewnętrzną rotację podudzia.</strong> <br> <a href= https://www.anatomik.pl/lekcje/miesien-posladkowy-maly/. target=_blank>Więcej Informacji.</a>`  
+}
